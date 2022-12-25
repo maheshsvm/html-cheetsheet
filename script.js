3939cd7ff2bdcba5a5ba8e8b33043ff46c60928b
Mahesh Sharma <maheshkaushik04611@gmail.com>
2022-12-25 17:39:46 +0530

    modified UI

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ const fillHeadings = () => {
     }
 }
 
-fillHeadings()
+// fillHeadings()
+
+const run = () => {
+    let heading = document.getElementById("heading-section");
+    for (let i = 1; i <= 6; i++) {
+
+        heading.innerHTML += `<div class="code-block"><h3>&lt;h${i}&gt; Tag</h3>
+        <pre class="language-html"><code>&lt;h${i}&gt;Heading ${i}&lt;/h${i}&gt;</code></div>`
+    }
+}
+
+run();
