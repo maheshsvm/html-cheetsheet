0e6376172fe4eb59f4d6c9212fa66f0f922ee1e0
Mahesh Sharma <maheshkaushik04611@gmail.com>
2022-12-26 13:45:05 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
 const fillHeadings = () => {
-    let codeBlock = document.getElementById("headings");
-    for (let i = 1; i <= 6; i++) {
-        codeBlock.innerHTML += `<h3>&lt;h${i}&gt; Tag</h3>
-        <pre class="language-html"><code>&lt;h${i}&gt;Heading ${i}&lt;/h${i}&gt;</code>`
-    }
-}
-
-// fillHeadings()
-
-const run = () => {
     let heading = document.getElementById("heading-section");
     for (let i = 1; i <= 6; i++) {
 
@@ -17,4 +7,4 @@ const run = () => {
     }
 }
 
-run();
+fillHeadings();
